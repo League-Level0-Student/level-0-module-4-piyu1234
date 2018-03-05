f14de1ca66c5a502d33d4f6ff38270b5b6947b57
piyu1234 <league@WTS10.attlocal.net>
2018-03-04 17:30:58 -0800

    working on program fruit quiz

--- src/FruitQuiz.java
+++ src/FruitQuiz.java
@@ -15,15 +15,19 @@ public class FruitQuiz extends KeyAdapter {
 	void makeQuestions() {
 		question1 = new JLabel("<html>Which is not a real fruit? <br> A: Dragon Fruit <br> B: Durian <br> C: Crazyberry</html>");
 		// 11. Make another question called "question2".  Use question1 above as a guide.
+		question2 = new JLabel("<html>Which is not a real plant? <br> A: rosemary <br> B: leafo <br> C: mint</html>");
+		
 	}
 
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		int keyCode = arg0.getKeyCode();
 		// 1. Print out the key code variable
-
-		// 2. Make 3 int variables that hold the key codes for A, b, and C
-		
+System. out.println(keyCode);
+		// 2. Make 3 int variables that hold the key codes for A, B, and C
+int key1= 65;
+int key2 = 66;
+int key3 = 67;
 		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The questions must be in reverse order from top to bottom to work properly
 		
 		// 12. If question2 is showing,
@@ -33,11 +37,15 @@ public class FruitQuiz extends KeyAdapter {
 			
 		if (question1.isShowing()) {
 			// 3. If they selected the right fruit, do steps 4 and 7
-			
+			if (keyCode == 66) {
+		correct();	
+		
+			}
+			else { incorrect();}
 				// 4. Call the correct() method
 				
 				// 7. Use the nextQuestion() method to go to question2
-			
+			nextQuestion(question2);
 			
 			// 8. else (if they touched something else)
 				
@@ -52,13 +60,13 @@ public class FruitQuiz extends KeyAdapter {
 		// the default package. It must be a .wav file. 
 		// There are lots on freesound.org
 		// 6. Use the playSound method to play your sound
-
+		playSound("correct.wav"); 
 
 	}
 
 	private void incorrect() {
 		// 10. Find a sound for wrong answers and put it in the default package. Use the playSound method to play it.
-
+		playSound("beeps.wav");
 	}
 
 	private void nextQuestion(JLabel newQuestion) {
