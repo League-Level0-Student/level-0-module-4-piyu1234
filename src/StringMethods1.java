80d94a08c7a6edf353264e32b0a3ae042733e1cf
Meijia He <league@WTS-iMac-11.attlocal.net>
2018-03-25 17:30:40 -0700

    String Methods 1 and 2

--- src/StringMethods1.java
+++ src/StringMethods1.java
@@ -7,24 +7,38 @@ public class StringMethods1 {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-
+		String AwesomeGirl = "Gymnast";
 		// 2. Print the 3rd char of your String to the console.
+		
+		System.out.println(AwesomeGirl.charAt(2)); 
+	
 		//    HINT: .charAt
 		
 		// 3. Print the length of your String to the console.
+		
+		System.out.println(AwesomeGirl.length());
 		//    HINT: .length
 
 		// 4. Using a for loop, print one char at time to the console.
+		for (int i = 0; i < 7; i++) {  
+			AwesomeGirl.charAt(i);
+			
+		}
 		//    HINT: use .length() to determine how many loops
 
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
+		
+		for (int i = 0; i <AwesomeGirl.length() ; i++) {
+			if (AwesomeGirl.charAt(i) == 'n')   {
+				System.out.println(i);
+		}
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
 		
 	}
 }
-
+}
 
--- src/StringMethods2.java
+++ src/StringMethods2.java
@@ -1,24 +1,35 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
+         public class StringMethods2 {
+		public static void main(String[] args) {
+			
+			// 1. Create a String variable and initialize it with
+			//    random upper and lower case characters.
+String RandomWord = "RAnDoM ";
 
-public class StringMethods2 {
-	public static void main(String[] args) {
-		
-		// 1. Create a String variable and initialize it with
-		//    random upper and lower case characters.
+			// 2. Print your String to the console in upper case.
+			System.out.println(RandomWord.toUpperCase());
+			// 3. Print your String to the console in lower case.
+System.out.println(RandomWord.toLowerCase());
+			// 4. Print the first 3 char's of your String
+RandomWord.substring(2, 4);
+System.out.println(RandomWord.substring(2, 4));
+			//    HINT: .substring(start,end)
 
-		// 2. Print your String to the console in upper case.
-		
-		// 3. Print your String to the console in lower case.
+			
+			// 5. Print a single char somewhere in the middle of your String
+        //    REMINDER: char's in string start at index 0  
+System.out.println(RandomWord.charAt(4));
 
-		// 4. Print the first 3 char's of your String
-		//    HINT: .substring(start,end)
-		
-		// 5. Print a single char somewhere in the middle of your String
-		//    REMINDER: char's in string start at index 0
-		
-		// 6. BONUS -- print the LAST 3 char's of your string using
-		//        .length() to determine WHERE the last 3 char's are located.
+			// 6. BONUS -- print the LAST 3 char's of your string using
+			//        .length() to determine WHERE the last 3 char's are located.
+int bigPos= RandomWord.length()-3;
+System.out.println("The last 3 characters are at");
+System.out.println(bigPos);
+System.out.println(bigPos+1);
+System.out.println(bigPos+2);
+		}
 	}
-}
+
